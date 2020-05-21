ba2ada1f4f027c0d77c6371e5963a1067dea4b85
Christelle M. Colin-Leitzinger <christelleleitzinger@gmail.com>
2020-05-21 09:37:16 -0400

    Initial commit
    add gitignore and start loading files

---
+++ 1.load_file.R
@@ -0,0 +1,63 @@
+# Packages
+library(tidyverse)
+
+# I  ### Load data
+
+path <- fs::path("","Volumes","Peres_Research")
+#-----------------------------------------------------------------------------------------------------------------
+clinical_data <-
+  read_csv(paste0(path,
+      "/K99_R00/Image analysis data/AACES and NCOCS data/aaces_ncocs_03042020.csv"
+    )) %>% 
+  select(c("avatar_id","TCC_ID","Date_of_Birth", "Gender", "Ethnicity", "Race"))
+#-----------------------------------------------------------------------------------------------------------------
+cases_match <-
+  readxl::read_xlsx(paste0(path,
+      "/K99_R00/Image analysis data/AACES and NCOCS data/Case matches_12312019.xlsx"
+    ))
+#-----------------------------------------------------------------------------------------------------------------
+ROIimmune_marker <-
+  readxl::read_xlsx(paste0(path,
+      "/K99_R00/Image analysis data/Immune marker count data/Peres P1 ROI Analysis with location_updated 1-24-2020.xlsx"
+    ))
+#-----------------------------------------------------------------------------------------------------------------
+TMAimmune_marker <-
+  readxl::read_xlsx(paste0(path,
+      "/K99_R00/Image analysis data/Immune marker count data/Peres P1 AACES 2017 TMA Summary.xlsx"
+  ))
+TMA2immune_marker <-
+  readxl::read_xlsx(paste0(path,
+      "/K99_R00/Image analysis data/Immune marker count data/Peres P1 AACES 2018 TMA Summary.xlsx"
+  ))
+#-----------------------------------------------------------------------------------------------------------------
+coor_immune_marker <-
+  read_csv(paste0(path,
+      "/K99_R00/Image analysis data/Per-Cell Object Data Table - Image Analysis/TMA AACES 2017/Peres_P1_AACES_TMA 2017_[1,A].tif_51355_job32199.object_results.csv"
+  ))
+#-----------------------------------------------------------------------------------------------------------------
+TMA_remove <-
+  read_csv(paste0(path,
+                  "/Christelle Colin-Leitzinger/IF_AACES_NCOCS/Subject_IDs to remove from TMA.csv"
+  ))
+#-----------------------------------------------------------------------------------------------------------------
+common_ROITMA_IDs <-
+  read_csv(paste0(path,
+                  "/Christelle Colin-Leitzinger/IF_AACES_NCOCS/Subject_IDs common TMA ROI.csv"
+  ))
+#-----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+write.csv(a, paste0(path, "/Christelle Colin-Leitzinger/IF_AACES_NCOCS/Subject_IDs to remove from TMA.csv"))
+
+write.csv(a, paste0(path, "/Christelle Colin-Leitzinger/IF_AACES_NCOCS/Subject_IDs common TMA ROI.csv"))
+
+
+
+
+
+
