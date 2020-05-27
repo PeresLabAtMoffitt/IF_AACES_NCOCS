72b8e269a061a533ec51fa56fa6098e782ff56cb
chrisleitzinger <christelleleitzinger@gmail.com>
2020-05-27 17:04:54 -0400

    Create summary table tumor vs stroma

--- 2.data_miming.R
+++ 2.data_miming.R
@@ -3,29 +3,77 @@
 # for the TMA cores, intratumoral ROIs, and peripheral ROIs. 
 # Also assess the variation by case in terms of the % tumor and % stroma. 
 
-ROI_tumor
-ROI_stroma
+
 
 ROI_global <- merge.data.frame(ROI_tumor, ROI_stroma,
                                by.x = "image_tag", by.y = "image_tag",
-                               all = TRUE)
+                               all = TRUE) %>% 
+  mutate(total_cell_number = `Tumor: Total Cells` + `Stroma: Total Cells`) %>% 
+  mutate(percent_tumor = round((`Tumor: Total Cells` / total_cell_number)*100, 2)) %>% 
+  mutate(percent_stroma = round((`Stroma: Total Cells` / total_cell_number)*100, 2)) 
 
 TMA_global <- merge.data.frame(TMA_tumor, TMA_stroma,
                                by.x = "image_tag", by.y = "image_tag",
-                               all = TRUE)
+                               all = TRUE) %>% 
+  mutate(total_cell_number = `Tumor: Total Cells` + `Stroma: Total Cells`) %>% 
+  mutate(percent_tumor = round((`Tumor: Total Cells` / total_cell_number)*100, 2)) %>% 
+  mutate(percent_stroma = round((`Stroma: Total Cells` / total_cell_number)*100, 2)) 
 
 
-table <- matrix(c("Tumor", "Stroma", "Total",
-                  
-  
-), ncol = , byrow = TRUE
-  
-)
-
-ROI_global <- ROI_global %>% 
-  mutate(total_cell_number = ROI_global$`Tumor: Total Cells` + ROI_global$`Stroma: Total Cells`)
-TMA_global <- TMA_global %>% 
-  mutate(total_cell_number = `Tumor: Total Cells` + `Stroma: Total Cells`)
+# ROI_global <- ROI_global %>% 
+#   mutate(total_cell_number = `Tumor: Total Cells` + `Stroma: Total Cells`) %>% 
+#   mutate(percent_tumor = round((`Tumor: Total Cells` / total_cell_number)*100, 2)) %>% 
+#   mutate(percent_stroma = round((`Stroma: Total Cells` / total_cell_number)*100, 2)) 
+# TMA_global <- TMA_global %>% 
+#   mutate(total_cell_number = `Tumor: Total Cells` + `Stroma: Total Cells`) %>% 
+#   mutate(percent_tumor = round((`Tumor: Total Cells` / total_cell_number)*100, 2)) %>% 
+#   mutate(percent_stroma = round((`Stroma: Total Cells` / total_cell_number)*100, 2)) 
 
 
-
+table <- matrix(c("", "Tumor", "Stroma",
+                  "TMA", "", "",
+                  "mean", round(mean(TMA_global$percent_tumor),2), round(mean(TMA_global$percent_stroma),2),
+                  "median", median(TMA_global$percent_tumor), median(TMA_global$percent_stroma),
+                  "range", 
+                  paste(range(TMA_global$percent_tumor)[1],range(TMA_global$percent_tumor)[2], sep = "-"), 
+                  paste(range(TMA_global$percent_stroma)[1],range(TMA_global$percent_stroma)[2], sep = "-"),
+                  "ROIs", "", "",
+                  "intratumoral", "", "",
+                  "mean",
+                  round(mean(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "i")$percent_tumor),2),
+                  round(mean(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "i")$percent_stroma),2),
+                  "median",
+                  median(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "i")$percent_tumor),
+                  median(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "i")$percent_stroma),
+                  "range",
+                  paste(
+                    range(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "i")$percent_tumor)[1],
+                    range(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "i")$percent_tumor)[2],
+                    sep = "-"
+                  ),
+                  paste(
+                    range(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "i")$percent_stroma)[1],
+                    range(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "i")$percent_stroma)[2],
+                    sep = "-"
+                  ),
+                  "peripheral", "", "",
+                  "mean",
+                  round(mean(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "p")$percent_tumor),2),
+                  round(mean(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "p")$percent_stroma),2),
+                  "median",
+                  median(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "p")$percent_tumor),
+                  median(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "p")$percent_stroma),
+                  "range",
+                  paste(
+                    range(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "p")$percent_tumor)[1],
+                    range(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "p")$percent_tumor)[2],
+                    sep = "-"
+                  ),
+                  paste(
+                    range(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "p")$percent_stroma)[1],
+                    range(x= filter(ROI_global, `Intratumoral (i) vs Peripheral (p).x` == "p")$percent_stroma)[2],
+                    sep = "-"
+                  )
+), ncol = 3, byrow = TRUE)
+write.csv(table, 
+          paste0(path, "/Christelle Colin-Leitzinger/IF_AACES_NCOCS/Summary tumor, stroma in TMAs and ROIs.csv"))
