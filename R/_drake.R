1d0f9ea0c8e4918b7baf683f282d154c128dd2da
chrisleitzinger <christelleleitzinger@gmail.com>
2020-05-28 13:20:37 -0400

    Update colnames and start setup drake

---
+++ R/_drake.R
@@ -0,0 +1,15 @@
+# For help with drake
+# https://ropenscilabs.github.io/drake-manual/projects.html#safer-interactivity
+# and
+# https://ropensci.github.io/drake/reference/r_make.html
+
+# Load your packages from packages.R
+source("R/packages.R")
+# Load the code as function that drake need to run
+source("R/function.R") 
+# Load the plan that drake has to execute
+source("R/plan.R")    
+
+# End drake
+config <- drake_config(plan, parallelism = "future", jobs = 4, verbose = 1)
+if (!interactive()) config
