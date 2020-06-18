bd53f6c5373841b6336146d68551bb0b003ad3d9
chrisleitzinger <christelleleitzinger@gmail.com>
2020-06-18 09:23:16 -0400

    Add comments, reorganize code

--- R/2.data_cleaning.R
+++ R/2.data_cleaning.R
@@ -37,9 +37,9 @@ ROI_global <- merge.data.frame(ROI_tumor, ROI_stroma %>% select(-c("intratumoral
                                all = TRUE) %>% 
   mutate(total_cell_number = tumor_total_cells + stroma_total_cells
          ) %>% 
-  mutate(percent_tumor = round((tumor_total_cells / total_cell_number)*100, 2)
+  mutate(percent_tumor = round((tumor_total_cells / total_cell_number)*100, 2) # Calculate percent of tumor cell
          ) %>% 
-  mutate(percent_stroma = round((stroma_total_cells / total_cell_number)*100, 2)
+  mutate(percent_stroma = round((stroma_total_cells / total_cell_number)*100, 2) # Calculate percent of stromal cell
          ) %>% 
   mutate_at(("intratumoral_i_vs_peripheral_p_"), ~ case_when(
     intratumoral_i_vs_peripheral_p_ == "p" ~ "Peripheral",
@@ -47,7 +47,7 @@ ROI_global <- merge.data.frame(ROI_tumor, ROI_stroma %>% select(-c("intratumoral
     ) %>% 
   mutate(suid = factor(suid)) %>% 
   select(suid, everything()) %>% 
-  mutate(CD3_tumor_mm2 = (tumor_cd3_opal_650_positive_cells/tumor_area_analyzed_mm2_)) %>% 
+  mutate(CD3_tumor_mm2 = (tumor_cd3_opal_650_positive_cells/tumor_area_analyzed_mm2_)) %>% # density of marker per mm2
   mutate(CD3_stroma_mm2 = (stroma_cd3_opal_650_positive_cells/stroma_area_analyzed_mm2_)) %>% 
   mutate(CD3_CD8_tumor_mm2 = (tumor_cd3plus_cd8plus_cells/tumor_area_analyzed_mm2_)) %>% 
   mutate(CD3_CD8_stroma_mm2 = (stroma_cd3plus_cd8plus_cells/stroma_area_analyzed_mm2_)) %>% 
@@ -58,7 +58,7 @@ ROI_global <- merge.data.frame(ROI_tumor, ROI_stroma %>% select(-c("intratumoral
   mutate(CD11b_CD15_tumor_mm2 = (tumor_cd11bplus_cd15plus_cells/tumor_area_analyzed_mm2_)) %>% 
   mutate(CD11b_CD15_stroma_mm2 = stroma_cd11bplus_cd15plus_cells/stroma_area_analyzed_mm2_) %>% 
   
-  mutate(CD3perc_tumor_mm2 = (tumor_percent_cd3_opal_650_positive_cells/tumor_area_analyzed_mm2_)) %>%
+  mutate(CD3perc_tumor_mm2 = (tumor_percent_cd3_opal_650_positive_cells/tumor_area_analyzed_mm2_)) %>% # percent of marker per mm2
   mutate(CD3perc_stroma_mm2 = (stroma_percent_cd3_opal_650_positive_cells/stroma_area_analyzed_mm2_)) %>%
   mutate(CD3_CD8perc_tumor_mm2 = (tumor_percent_cd3plus_cd8plus_positive_cells/tumor_area_analyzed_mm2_)) %>%
   mutate(CD3_CD8perc_stroma_mm2 = (stroma_percent_cd3plus_cd8plus_positive_cells/stroma_area_analyzed_mm2_)) %>%
@@ -103,23 +103,23 @@ TMA_global <- merge.data.frame(TMA_tumor, TMA_stroma %>% select(-suid),
   mutate(CD11b_CD15perc_tumor_mm2 = (tumor_percent_cd11bplus_cd15plus_positive_cells/tumor_area_analyzed_mm2_)) %>%
   mutate(CD11b_CD15perc_stroma_mm2 = stroma_percent_cd11bplus_cd15plus_positive_cells/stroma_area_analyzed_mm2_)
 
+# Look at the variation between each patient and the global mean # Should we mot compare Black and White?
+# Here compare the mean of % cells to global study % cells
 variations_TMA <- group_by(TMA_global, suid) %>% 
   summarize(mean_tumor = mean(percent_tumor), mean_stroma = mean(percent_stroma)) %>% 
-  mutate(ID = seq(1:nrow(.)))# %>% 
-# mutate(suid = factor(suid))
-
-variations_ROIip <- group_by(ROI_global, suid, intratumoral_i_vs_peripheral_p_) %>% 
-  summarize(mean_tumor = mean(percent_tumor), mean_stroma = mean(percent_stroma))
-setDT(variations_ROIip)[, ID := .GRP, .(suid)]
-
-variations_ROI <- group_by(variations_ROIip, suid) %>% 
-  summarize(mean_tumor = mean(mean_tumor), mean_stroma = mean(mean_stroma)) %>% 
   mutate(ID = seq(1:nrow(.)))
-
 variations_TMA$tumor_variation <- variations_TMA$mean_tumor - mean(TMA_global$percent_tumor)
 variations_TMA$stroma_variation <- variations_TMA$mean_stroma - mean(TMA_global$percent_stroma)
+
+variations_ROIip <- group_by(ROI_global, suid, intratumoral_i_vs_peripheral_p_) %>% # mean of % cells separated by intra or perip
+  summarize(mean_tumor = mean(percent_tumor), mean_stroma = mean(percent_stroma))
+setDT(variations_ROIip)[, ID := .GRP, .(suid)]
 variations_ROIip$tumor_variation <- variations_ROIip$mean_tumor - mean(ROI_global$percent_tumor)
 variations_ROIip$stroma_variation <- variations_ROIip$mean_stroma - mean(ROI_global$percent_stroma)
+
+variations_ROI <- group_by(variations_ROIip, suid) %>% 
+  summarize(mean_tumor = mean(mean_tumor), mean_stroma = mean(mean_stroma)) %>% # mean of % cells merging intra or perip
+  mutate(ID = seq(1:nrow(.)))
 variations_ROI$tumor_variation <- variations_ROI$mean_tumor - mean(ROI_global$percent_tumor)
 variations_ROI$stroma_variation <- variations_ROI$mean_stroma - mean(ROI_global$percent_stroma)
 
